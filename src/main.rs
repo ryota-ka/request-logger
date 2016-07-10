dedfb95859bec9dc930eca4fb89a5f07028e4f82
Ryota Kameoka <kameoka.ryota@gmail.com>
2016-07-11 00:43:12 +0900

    Implement server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+extern crate iron;
+extern crate request_logger;
+
+use request_logger::RequestLogger;
+use iron::prelude::*;
+use iron::status;
+
 fn main() {
-    println!("Hello, world!");
+    fn hello(_: &mut Request) -> IronResult<Response> {
+        Ok(Response::with((status::Ok, "Hello")))
+    }
+
+    let mut chain = Chain::new(hello);
+    chain.link_before(RequestLogger);
+    Iron::new(chain).http("localhost:8000").unwrap();
+    println!("Listening on port 8000...")
 }
